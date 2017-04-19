ae2aefa576163bd7488385250eeb0c7571b5c31d
songxu <songxu@6tiantian.com>
2017-04-19 14:43:38 +0800

    添加了lambda支持和eventbus框架

--- app/src/main/java/com/bupt/androidsip/activity/MainActivity.java
+++ app/src/main/java/com/bupt/androidsip/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.bupt.androidsip.activity;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.bupt.androidsip.R;
 
@@ -11,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
