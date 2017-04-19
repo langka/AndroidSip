d6bc1769689e7b7cc18eec452cf35b4dc18c1d9b
songxu <songxu@6tiantian.com>
2017-04-19 14:18:56 +0800

    添加基础的activity

---
+++ app/src/main/java/com/bupt/androidsip/activity/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.bupt.androidsip.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by lenovo on 2017/4/19.
+ */
+
+public class BaseActivity extends FragmentActivity {
+}
---
+++ app/src/main/java/com/bupt/androidsip/fragment/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.bupt.androidsip.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by lenovo on 2017/4/19.
+ */
+
+public class BaseFragment extends Fragment {
+}
