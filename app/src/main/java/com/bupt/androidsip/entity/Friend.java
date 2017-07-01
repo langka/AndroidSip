839b35353178fcf27fc1b082f16eff0283627f1f
HYWang <whyang666@gmail.com>
2017-07-01 16:49:57 +0800

    好友界面

---
+++ app/src/main/java/com/bupt/androidsip/entity/Friend.java
@@ -0,0 +1,8 @@
+package com.bupt.androidsip.entity;
+
+/**
+ * Created by WHY on 2017/7/1.
+ */
+
+public class Friend {
+}
---
+++ app/src/main/java/com/bupt/androidsip/fragment/FriendFragment.java
@@ -0,0 +1,8 @@
+package com.bupt.androidsip.fragment;
+
+/**
+ * Created by WHY on 2017/7/1.
+ */
+
+public class FriendFragment {
+}
