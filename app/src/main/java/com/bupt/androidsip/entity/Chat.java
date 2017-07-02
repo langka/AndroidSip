bd98e1783cc5012a0a8f4113461781137d137301
HYWang <whyang666@gmail.com>
2017-07-02 23:52:09 +0800

    聊天列表不完整

--- app/src/main/java/com/bupt/androidsip/entity/Chat.java
+++ app/src/main/java/com/bupt/androidsip/entity/Chat.java
@@ -5,15 +5,27 @@ package com.bupt.androidsip.entity;
  */
 
 public class Friend {
+
+    /**
+     * @String name：
+     *  用户名
+     * @String headImageURL
+     *  头像的URL
+     * @int onlineStatue
+     *  在线状态
+     *      0：离线
+     *      1：在线
+     *      2：忙碌
+     */
     public String name;
     public String headImageURL;
-    public String onlineStatue;
+    public int onlineStatue;
     public String signature;
 
     public Friend() {
     }
 
-    public Friend(String name, String headImageURL, String onlineStatue, String signature) {
+    public Friend(String name, String headImageURL,  onlineStatue, String signature) {
         this.name = name;
         this.headImageURL = headImageURL;
         this.onlineStatue = onlineStatue;
@@ -29,7 +41,7 @@ public class Friend {
         return headImageURL;
     }
 
-    public String getOnlineStatue() {
+    public int getOnlineStatue() {
         return onlineStatue;
     }
 
