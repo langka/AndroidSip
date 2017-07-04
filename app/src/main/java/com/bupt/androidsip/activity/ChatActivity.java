7aad6ae03655aa3ed2669da6a156fe0e5612c3ca
Yingkai Luan <vita.nove.yl@gmail.com>
2017-07-04 20:07:30 +0800

    完成chat的listview adapter 更新一些UI

--- app/src/main/java/com/bupt/androidsip/activity/ChatActivity.java
+++ app/src/main/java/com/bupt/androidsip/activity/ChatActivity.java
@@ -1,23 +1,36 @@
 package com.bupt.androidsip.activity;
 
+import android.content.Context;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.bupt.androidsip.R;
+import com.bupt.androidsip.entity.Message;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.R.attr.name;
 
 /**
  * Created by vita-nove on 04/07/2017.
@@ -45,6 +58,10 @@ public class ChatActivity extends BaseActivity {
     @BindView(R.id.msg_listview)
     ListView msgListview;
 
+    private MessageAdapter msgAdapter = null;
+    private List<Message> messages = null;
+    private Context context;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,8 +69,17 @@ public class ChatActivity extends BaseActivity {
         ButterKnife.bind(this);
         // TODO: 04/07/2017 徐日天要修正header被顶上去的问题 
         // controlKeyboardLayout(root, headerContainer);
-        headerContainer.scrollBy(100, 200);
+        context = ChatActivity.this;
+        messages = new LinkedList<Message>();
+        msgAdapter = new MessageAdapter((LinkedList<Message>) messages, context);
+        msgListview.setAdapter(msgAdapter);
+        getHeaderDivider().setVisibility(View.GONE);
+        enableLeftImage(R.drawable.ic_arrow_back_24px, e -> finish());
+
+        msgAdapter.add(new Message("徐松", R.drawable.xusong, "我爱吃西瓜", true));
+
     }
+
 
     private void controlKeyboardLayout(final View root, final View targetView) {
         root.getViewTreeObserver().addOnGlobalLayoutListener(
@@ -77,5 +103,67 @@ public class ChatActivity extends BaseActivity {
                         }
                     }
                 });
+    }
+
+
+    static class MessageAdapter extends BaseAdapter {
+        private LinkedList<Message> messages;
+        private Context context;
+
+        public void add(Message message) {
+
+            messages.add(message);
+            notifyDataSetChanged();
+        }
+
+        public MessageAdapter() {
+        }
+
+        public MessageAdapter(LinkedList<Message> messages, Context context) {
+            this.messages = messages;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return messages.size();
+        }
+
+        @Override
+        public Message getItem(int position) {
+            return messages.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context).inflate(R.layout.item_chat_msg_left,
+                        parent, false);
+                holder = new ViewHolder();
+                holder.avatar = (CircleImageView) convertView.findViewById(R.id.item_avatar);
+                holder.name = (TextView) convertView.findViewById(R.id.item_name);
+                holder.msg = (TextView) convertView.findViewById(R.id.item_msg);
+                convertView.setTag(holder);
+            } else
+                holder = (ViewHolder) convertView.getTag();
+
+            holder.avatar.setImageResource(R.drawable.xusong);
+            holder.msg.setText("西瓜爱吃我");
+            holder.name.setText("徐日天");
+            return convertView;
+        }
+
+        private class ViewHolder {
+            CircleImageView avatar;
+            TextView name;
+            TextView msg;
+        }
+
     }
 }
---
+++ app/src/main/java/com/bupt/androidsip/entity/Message.java
@@ -0,0 +1,40 @@
+package com.bupt.androidsip.entity;
+
+/**
+ * Created by vita-nove on 04/07/2017.
+ */
+
+public class Message {
+    private String name;
+    private int headImageURL;
+    private String message;
+    private boolean isFromMe;
+
+    public boolean getIsFromMe() {
+        return isFromMe;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHeadImageURL() {
+        return headImageURL;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Message() {
+    }
+
+    public Message(String name, int headImageURL, String message, boolean isFromMe) {
+        this.name = name;
+        this.headImageURL = headImageURL;
+        this.message = message;
+        this.isFromMe = isFromMe;
+    }
+
+
+}
