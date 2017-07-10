ba59f9f395508c4bd31172657d3c2d996b09e0e3
HYWang <whyang666@gmail.com>
2017-07-10 16:23:54 +0800

    初步好友管理

---
+++ app/src/main/java/com/bupt/androidsip/activity/AddFriendActivity.java
@@ -0,0 +1,8 @@
+package com.bupt.androidsip.activity;
+
+/**
+ * Created by WHY on 2017/7/10.
+ */
+
+public class AddFriendActivity {
+}
---
+++ app/src/main/java/com/bupt/androidsip/entity/response/SipSearchResponse.java
@@ -0,0 +1,8 @@
+package com.bupt.androidsip.entity.response;
+
+/**
+ * Created by WHY on 2017/7/10.
+ */
+
+public class SipSearchResponse extends BaseResponse {
+}
